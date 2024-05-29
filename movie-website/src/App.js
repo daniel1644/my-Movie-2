c2b90266324e2f7342ed25361bc310ff66dc0bd6
danielbeyene <daniel.beyene@student.moringashcool.com>
2024-05-29 19:13:21 +0300

    final

--- movie-website/src/App.js
+++ movie-website/src/App.js
@@ -1,49 +1,85 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import './App.css';
 import MovieList from './Components/MovieList';
-import SearchBar from './Components/SearchBar';
-import AddMovieForm from './Components/AddMovieForm';
+import MovieListHeading from './Components/MovieListHeading';
+import SearchBox from './Components/SearchBox';
+import AddFavourites from './Components/AddFavourites';
+import RemoveFavourites from './Components/RemoveFavourites';
 
-function App() {
-  const [movies, setMovies] = useState([]);
-  const [filteredMovies, setFilteredMovies] = useState([]);
+const App = () => {
+	const [movies, setMovies] = useState([]);
+	const [favourites, setFavourites] = useState([]);
+	const [searchValue, setSearchValue] = useState('');
 
-  useEffect(() => {
-    const fetchMovies = async () => {
-      try {
-        const response = await axios.get('https://api.myjson.online/v1/records/78a0a148-bbb0-4b2f-8038-e01c92b6a9d7');
-        // Assuming the response data is an array of movie objects
-        setMovies(response.data);
-        setFilteredMovies(response.data);
-      } catch (error) {
-        console.error('Error fetching movies:', error);
-      }
-    };
+	const getMovieRequest = async (searchValue) => {
+		const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=263d22d8`;
 
-    fetchMovies();
-  }, []);
+		const response = await fetch(url);
+		const responseJson = await response.json();
 
-  const handleSearch = searchTerm => {
-    const filtered = movies.filter(movie =>
-      movie.name.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-    setFilteredMovies(filtered);
-  };
+		if (responseJson.Search) {
+			setMovies(responseJson.Search);
+		}
+	};
 
-  const handleAddMovie = newMovie => {
-    setMovies([...movies, newMovie]);
-    setFilteredMovies([...filteredMovies, newMovie]);
-  };
+	useEffect(() => {
+		getMovieRequest(searchValue);
+	}, [searchValue]);
 
-  return (
-    <div>
-      <h1>Movie Library</h1>
-      <SearchBar onSearch={handleSearch} />
-      <AddMovieForm onAddMovie={handleAddMovie} />
-      {/* Ensure movies prop is initialized with an array */}
-      <MovieList movies={filteredMovies} />
-    </div>
-  );
-}
+	useEffect(() => {
+		const movieFavourites = JSON.parse(
+			localStorage.getItem('react-movie-app-favourites')
+		);
+
+		if (movieFavourites) {
+			setFavourites(movieFavourites);
+		}
+	}, []);
+
+	const saveToLocalStorage = (items) => {
+		localStorage.setItem('react-movie-app-favourites', JSON.stringify(items));
+	};
+
+	const addFavouriteMovie = (movie) => {
+		const newFavouriteList = [...favourites, movie];
+		setFavourites(newFavouriteList);
+		saveToLocalStorage(newFavouriteList);
+	};
+
+	const removeFavouriteMovie = (movie) => {
+		const newFavouriteList = favourites.filter(
+			(favourite) => favourite.imdbID !== movie.imdbID
+		);
+
+		setFavourites(newFavouriteList);
+		saveToLocalStorage(newFavouriteList);
+	};
+
+	return (
+		<div className='container-fluid movie-app'>
+			<div className='row d-flex align-items-center mt-4 mb-4'>
+				<MovieListHeading heading='Movies' />
+				<SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+			</div>
+			<div className='row'>
+				<MovieList
+					movies={movies}
+					handleFavouritesClick={addFavouriteMovie}
+					favouriteComponent={AddFavourites}
+				/>
+			</div>
+			<div className='row d-flex align-items-center mt-4 mb-4'>
+				<MovieListHeading heading='Favourites' />
+			</div>
+			<div className='row'>
+				<MovieList
+					movies={favourites}
+					handleFavouritesClick={removeFavouriteMovie}
+					favouriteComponent={RemoveFavourites}
+				/>
+			</div>
+		</div>
+	);
+};
 
 export default App;
